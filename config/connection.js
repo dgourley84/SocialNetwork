122e73c00b10c3bafaae23571c5c359efdbbb3c1
dgourley84 <81537606+dgourley84@users.noreply.github.com>
2022-12-18 10:43:39 +1000

    Adddition of user schema to have connection activate

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
-const { connect, connection } = require('mongoose');
+const mongoose = require('mongoose');
 
-
-connect('mongodb://127.0.0.1:27017/social_network_db', {
+// Wrap Mongoose around local connection to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/social_network_db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
+});
 
-// Export connection 
-module.exports = connection;
+// Export connection
+module.exports = mongoose.connection;
--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const db = require('./config/connection');
+const { User } = require('./models')
 // const routes = require('./routes');
+const PORT = process.env.PORT || 3001;
 const app = express();
-const PORT = 3001;
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // app.use(require("./routes"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/social_network_db",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
-app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
