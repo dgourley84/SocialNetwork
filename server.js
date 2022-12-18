229a80bfe6f090f0a770ce9d66a2a9a7197c0fba
dgourley84 <81537606+dgourley84@users.noreply.github.com>
2022-12-18 10:16:30 +1000

    set up of connection to MongoDB

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(require("./routes"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/social_network_db",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
