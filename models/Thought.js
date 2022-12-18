119f43f015fc94420064dbe8b5deea9695a53d2e
dgourley84 <81537606+dgourley84@users.noreply.github.com>
2022-12-18 12:33:57 +1000

    Completion of models and virtual

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model, Types } = require("mongoose");
+const reactionSchema = require("./Reaction.js")
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of reactions and replies on retrieval
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
